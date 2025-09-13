098d972f67e284839b2278a76944a9ed3ff45fc0
Blinomess <nikolaybudinov@mail.ru>
2025-09-13 15:11:45 +0300

    first version

---
+++ ComLineEm.py
@@ -0,0 +1,43 @@
+import os
+import socket
+class ComLineEm:
+    def __init__(self):
+        self.currentpath="~"
+        self.user = os.getlogin()
+        self.hostname = socket.gethostname()    
+
+    def run(self):
+        print("Введите help для получения списка команд")
+        print("-" * 50)
+        
+        while True:
+            command_line = input(f"{self.user}@{self.hostname}:{self.currentpath}$ ").split()
+                
+            command= command_line[0]
+            args=command_line[1:]
+            if command =='help' and not args:
+                print("Доступные команды: ls, cd, exit")
+                print("Для выхода введите 'exit'")
+            elif command == 'exit' and not args:
+                print("Выход из эмулятора")
+                break            
+            elif command == 'ls':
+                self.ls(args)
+            elif command == 'cd':
+                self.cd(args)            
+            else:
+                print(f"Ошибка: неизвестная команда '{command}'")
+                
+    def ls(self, args):
+        print(f"Команда: ls")
+        print(f"Аргументы: {args}")
+    
+    
+    def cd(self, args):
+        print(f"Команда: cd")
+        print(f"Аргументы: {args}")
+    
+
+if __name__ == "__main__":
+    shell = ComLineEm()
+    shell.run()
